5a6866e337c0679d49488d377bafdecd94661c0f
Zheng Ke <zq95521911@gmail.com>
2018-06-29 01:38:21 -0700

    add docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,10 @@
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 project = 'python_setup_sample'
 copyright = '2018, allen'
@@ -79,7 +83,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
