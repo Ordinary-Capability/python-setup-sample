ae24a4db13a99262d2f7e1b5d59cda0648058266
Zheng Ke <zq95521911@gmail.com>
2018-06-28 23:40:44 -0700

    add docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{bullet:0,has:0,index:0,item:0,line:0,list:0,modul:0,number:0,page:0,search:0,second:0,thi:0,too:0,two:0,uses:0},titles:["Welcome to python_setup_sample\u2019s documentation!"],titleterms:{document:0,indic:0,python_setup_sampl:0,tabl:0,welcom:0}})
